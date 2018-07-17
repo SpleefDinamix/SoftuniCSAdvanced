6af78e8e67db675b14b98f73d03da31269011267
Spleef Dinamix <pepi15032000@gmail.com>
2018-07-17 11:33:19 +0300

    Stacks Exercise

---
+++ StacksAndQueues/ReverseNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> reversedNumbers = new Stack<int>();
+
+            foreach (var number in numbers)
+            {
+                reversedNumbers.Push(number);
+            }
+
+            Console.WriteLine(String.Join(" ", reversedNumbers));
+        }
+    }
+}
